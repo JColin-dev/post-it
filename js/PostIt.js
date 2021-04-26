d6ac34463f91730a5c271c957d3241092b3a6380
JColin-dev <colin_dev@outlook.fr>
2021-04-26 15:44:10 +0200

    ajout css

--- js/PostIt.js
+++ js/PostIt.js
@@ -2,14 +2,14 @@ let myPostIt = new PostIt("un");
 
 myPostIt.display()
 
-myPostIt.move(200, 200)
+myPostIt.move(50, 50)
 myPostIt.display()
 
-myPostIt.resize(300, 300)
+myPostIt.resize(200, 200)
 myPostIt.display()
 
 myPostIt.modifText("Premier texte")
 myPostIt.display()
 
-myPostIt.modifColor("red")
+myPostIt.modifColor("cyan")
 myPostIt.display()
