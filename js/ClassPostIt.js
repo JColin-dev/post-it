7eb491451773b614b07f17c3620777a79f74405f
JColin-dev <colin_dev@outlook.fr>
2021-04-27 16:53:25 +0200

    ihm + événements

--- js/ClassPostIt.js
+++ js/ClassPostIt.js
@@ -6,34 +6,82 @@ class PostIt {
     x;
     y;
     text;
+    largeurInit;
+    hauteurInit;
+    sourisXInit;
+    sourisYInit;
 
     constructor(id) {
         this.id = id;
         this.Width = 176;
         this.Height = 176;
         this.Color = "blue";
-        this.x = 50;
-        this.y = 50;
+        this.x = 1020;
+        this.y = 20;
         this.text = "";
     }
 
     display() {
         let myPostIt = document.getElementById(this.id)
-        if(myPostIt === null) {
+        if (myPostIt === null) {
             myPostIt = document.createElement('div');
             myPostIt.id = this.id
             document.body.appendChild(myPostIt)
+
+            myPostIt.addEventListener('dragend', (e) => {
+                this.x = e.screenX - Math.floor(this.Width / 2)
+                this.y = e.screenY - Math.floor(this.Height / 2)
+                this.display()
+            })
         }
 
-        
-        myPostIt.draggable =true
+
+        myPostIt.draggable = true
         myPostIt.innerHTML = this.text
         myPostIt.style.position = "fixed"
         myPostIt.style.width = (+this.Width + "px")
         myPostIt.style.height = (+this.Height + "px")
-        myPostIt.style.backgroundColor = this.Color 
+        myPostIt.style.backgroundColor = this.Color
         myPostIt.style.left = this.x + "px"
         myPostIt.style.top = this.y + "px"
+
+        let menu = document.createElement('div')
+        menu.style.height = '20px'
+        menu.style.border = '1px solid black'
+        menu.style.position = 'absolute'
+        menu.style.width = "100px"
+        menu.style.bottom = '0'
+        menu.style.right = '0'
+        myPostIt.appendChild(menu)
+
+        let bout1 = document.createElement('i')
+        bout1.classList.add('fas')
+        bout1.classList.add('fa-arrows-alt')
+        bout1.addEventListener('click', (e) => {
+            move = true
+            e.stopPropagation()
+        })
+        menu.appendChild(bout1)
+        let bout2 = document.createElement('i')
+        bout2.classList.add('fas')
+        bout2.classList.add('fa-expand-alt')
+        bout2.addEventListener('click', (e) => {
+            resize = true
+            this.largeurInit = this.Width
+            this.hauteurInit = this.Height
+            this.sourisXInit = sourisX
+            this.sourisYInit = sourisY
+            e.stopPropagation()
+        })
+        menu.appendChild(bout2)
+        let bout3 = document.createElement('i')
+        bout3.classList.add('far')
+        bout3.classList.add('fa-edit')
+        bout3.addEventListener('click', (e) => {
+            edit = true
+            e.stopPropagation()
+        })
+        menu.appendChild(bout3)
     }
 
     move(newX, newY) {
@@ -53,5 +101,5 @@ class PostIt {
     modifColor(color) {
         this.Color = color
     }
-    
 }
+
--- js/PostIt.js
+++ js/PostIt.js
@@ -1,8 +1,15 @@
+let resize = false
+let move = false
+let edit = false
+let sourisX
+let sourisY
+
+
 let myPostIt = new PostIt("un");
 
 myPostIt.display()
 
-myPostIt.move(50, 50)
+/*myPostIt.move(50, 50)
 myPostIt.display()
 
 myPostIt.resize(200, 200)
@@ -11,5 +18,28 @@ myPostIt.display()
 myPostIt.modifText("Premier texte")
 myPostIt.display()
 
-myPostIt.modifColor("cyan")
+myPostIt.modifColor("darkblue")
 myPostIt.display()
+*/
+
+
+document.body.addEventListener('click', () => {
+    resize = false
+    move = false
+    edit = false
+})
+
+document.body.addEventListener('mousemove', (e) => {
+    sourisY = e.clientY
+    sourisX = e.clientX
+    if (move == true) {
+        myPostIt.move(e.clientX - 20, e.clientY - 20)
+        myPostIt.display()
+    } else if (resize == true) {
+        myPostIt.resize(myPostIt.largeurInit + (e.clientX - myPostIt.sourisXInit),
+            myPostIt.hauteurInit + 25 + (e.clientY - myPostIt.sourisYInit))
+        myPostIt.display()
+    }
+})
+
+
